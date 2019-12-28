6cb57e3cfb14c229e26c5f38d707b4541e78ccf9
Kat Lynch <kat.lynch@futurelearn.com>
2019-12-28 13:35:32 +0000

    get cookie from file

--- headers.py
+++ headers.py
@@ -1,15 +1,7 @@
-HEADERS = {
-    'cookie': 'xxx',
-    'accept-encoding': "gzip, deflate, br",
-    'accept-language': "en-US,en;q=0.9",
-    'cache-control': "no-cache",
-    'pragma': "no-cache",
-    'sec-fetch-mode': "navigate",
-    'sec-fetch-site': "none",
-    'sec-fetch-user': "?1",
-    'upgrade-insecure-requests': "1",
-    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"
-    }
+COOKIE_FILENAME = '.cookie'
+
+def read_cookie():
+    return open('.cookie', 'r').read().strip()
 
 def headers():
-    return HEADERS
+    return {'cookie': read_cookie()}
