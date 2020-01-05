6d9134e691493395cf3b7aaf7714321f2d585572
Kat Lynch <kat.lynch@futurelearn.com>
2020-01-05 11:20:06 +0000

    can generate and download matches

--- background.js
+++ background.js
@@ -1,24 +1,24 @@
-function getCookies(callback, domain='https://www.ancestry.co.uk') {
-  console.log("getcookies");
-  chrome.cookies.getAll({}, function(cookies) {
-    const cookieString = cookies.map((c) => {
-      Cookies.set(c.name, c.value);
-      return `${c.name}=${c.value}`
-    }).join(";");
-    callback(cookieString);
-  });
-}
+// function getCookies(callback, domain='https://www.ancestry.co.uk') {
+//   console.log("getcookies");
+//   chrome.cookies.getAll({}, function(cookies) {
+//     const cookieString = cookies.map((c) => {
+//       Cookies.set(c.name, c.value);
+//       return `${c.name}=${c.value}`
+//     }).join(";");
+//     callback(cookieString);
+//   });
+// }
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    console.log("about to getcookies");
-    getCookies(function(cookie) {
-      console.log(cookie);
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "cookie": cookie});
-    });
-
+// // Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // Send a message to the active tab
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    const activeTab = tabs[0];
+    const url = activeTab.url;
+    // TODO make this less brittle
+    const guid = url.split('?')[0].split('/')[5];
+    console.log("guid", guid);	
+    chrome.tabs.sendMessage(activeTab.id, {guid: guid});
   });
 });
--- content.js
+++ content.js
@@ -1,11 +1,18 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+chrome.runtime.onMessage.addListener(
+  (request, sender, sendResponse) => {
+    console.log("guid", request.guid);
 
-console.log(firstHref);
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', function() {
+        addGenerateButton(request.guid);
+      });
+    } else {
+      addGenerateButton(request.guid);
+    }
+  }
+)
 
 OUTPUT_FILENAME = 'matches.csv'
-
-
-USER_GUID = '34E617A6-092E-49C8-9A44-D1F8E9ABD255'
 
 FIELD_NAMES = ['displayName', 'subjectGender', 'testGuid', 'createdDate', 'note']
 RELATIONSHIP_FIELD_NAMES = ['meiosis', 'sharedCentimorgans', 'sharedSegments', 'confidence']
@@ -13,62 +20,115 @@ RELATIONSHIP_FIELD_NAMES = ['meiosis', 'sharedCentimorgans', 'sharedSegments', '
 MAX_TOTAL_RETRIES = 10
 MAX_RETRIES_PER_URL = 1
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+function addGenerateButton(guid) {
+  const id = 'js-ancestraid';
+  if (!$(`#${id}`).length) {
+    const input = $(`<div id=${id}>`);
+    input.prependTo($("body"));
 
-      console.log(firstHref);
-      console.log(request.cookie);
-      downloadMatches(request.cookie);
-    }
+    const buttonId = 'js-ancesteraid-generate-button';
+    const link = $(`<button class="filter ancBtn outline btnFilter iconAfter" id=${buttonId} >ANCESTRAID - click here to generate matches</button>`);
+    link.prependTo($(`#${id}`));
+    $(`#${buttonId}`).on('click', () => { generateMatches(guid) } );
   }
-);
+}
 
-function downloadMatches(cookie) {
-  const pageNumber = 0;
+function updateMessage(message) {
+  const buttonId = 'js-ancesteraid-generate-button';
+  $(`#${buttonId}`).html(`ANCESTRAID - ${message}`);
+}
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "clicked_browser_action" ) {
+//       downloadMatches(request.cookie);
+//     }
+//   }
+// );
+
+async function generateMatches(guid) {
+  updateMessage('generating...');
   const retries = 0;
   const retries_per_url = 0;
+  const moreMatches = true;
 
-  // while (true) {
-  console.log(`page ${pageNumber + 1}...`);
-  const url = buildUrl(USER_GUID, pageNumber);
-  var settings = {
+  let rows = [];
+  let pageNumber = 0;
+  let count = 0;
+  while (true) {
+    count++;
+    console.log(`page ${pageNumber + 1}...`);
+    updateMessage(`generating page ${pageNumber + 1}...`);
+    
+    const pageRows = await getMatches(pageNumber, guid);
+    if (!!pageRows.length) {
+      rows = rows.concat(pageRows)
+    } else {
+      break;
+    }
+    pageNumber++;
+  }
+
+  enableDownload(rows);
+}
+
+async function getMatches(pageNumber, guid, retry = 0) {
+  const url = buildUrl(guid, pageNumber);
+  // let rows = []
+  await $.ajax({
     "dataType": "json",
     "async": true,
     "crossDomain": true,
     "url": url,
     "method": "GET",
-    // "headers": { "cookie": cookie }
     "xhrFields": {
       "withCredentials": true
     },
-    success: (response) => { writeMatches(response) }
-  }
-
-  $.ajax(settings, (response) => {
-    console.log("response");
-    console.log(response);
+    success: (response) => {
+      const rows = getMatchRows(response);
+    },
+    error: (jqXHR, textStatus, errorThrown) => {
+      console.log('ERROR');
+      console.log('jqXHR:');
+      console.log(jqXHR);
+      console.log('textStatus:');
+      console.log(textStatus);
+      console.log('errorThrown:');
+      console.log(errorThrown);
+      if (retry > 2) {
+        throw "Too many errors"
+      } else {
+        const rows = getMatches(pageNumber, guid, retry++)
+      }
+    }
   });
-  // }
-
+  return rows;
 }
 
-function errorHandler() {
-  console.log("error");
+function getMatchRows(response) {
+  rows = []
+
+  response.matchGroups.map((matchGroup) => {
+    matchGroup.matches.forEach((match) => {
+      rows.push(match);
+    })
+  })
+
+  return rows;
+}
+
+function enableDownload(rows) {
+  csvwriter(rows, {fields: FIELD_NAMES}, (err, v) => {
+    $('#js-ancestraid').empty();
+    var text = v;
+
+    var fileBlob = new Blob([text], {type: "application/octet-binary"});
+
+    const link = $(`<button class="filter ancBtn outline btnFilter iconAfter"><a href=${URL.createObjectURL(fileBlob)} download=${OUTPUT_FILENAME}>ANCESTRAID - done - click to download</a></button>`);
+    link.prependTo($('#js-ancestraid'));
+  });
 }
 
 function buildUrl(userGuid, pageNumber) {
-  return `https://www.ancestry.co.uk/discoveryui-matchesservice/api/samples/${userGuid}/matchesv2?bookmarkdata={{"lastMatchesServicePageIdx":${pageNumber}}}`
+  return `https://www.ancestry.co.uk/discoveryui-matchesservice/api/samples/${userGuid}/matchesv2?bookmarkdata=%7B%22lastMatchesServicePageIdx%22%3A${pageNumber}%7D`
 }
-
-function writeMatches(response) {
-  console.log("response", response);
-}
-
-// function addButton(){
-//   var $input = $('<input type="button" value="new button" />');
-//   $input.prependTo($("body"));
-// }
-
-// addButton();
